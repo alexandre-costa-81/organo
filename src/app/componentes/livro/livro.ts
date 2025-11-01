e499f9024dd04545a4f53d24fcb1a21a39f863b9
Alexandre Costa <alexandre.costa@furg.br>
2025-11-01 01:33:33 -0300

    Add image display to Livro component; update livro.ts to include image URL

--- src/app/componentes/livro/livro.ts
+++ src/app/componentes/livro/livro.ts
@@ -10,5 +10,6 @@ export class Livro {
   livro = {
     titulo: 'O Alquimista',
     autoria: 'Paulo Coelho',
+    imagem: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUTadAgtHb677Fy6QBLxJtQ0wsT739BYdNIkc8ZNnzmveHIB-II5T2t3Gywp_XDKfx4-rEyaMbxZEZ94Q4DIFJxWMwOYEuNJzcbrez44cwqg&s=10',
   };
 }
