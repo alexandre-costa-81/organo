9b99148dc8ffcacad0ca63b0e6ca754a8efdfc1e
Alexandre Costa <alexandre.costa@furg.br>
2025-11-01 13:43:36 -0300

    Add styles and functionality for favorite button in Livro component

--- src/app/componentes/livro/livro.ts
+++ src/app/componentes/livro/livro.ts
@@ -10,6 +10,12 @@ export class Livro {
   livro = {
     titulo: 'O Alquimista',
     autoria: 'Paulo Coelho',
+    favorito: false,
+    genero: 'Ficção',
     imagem: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUTadAgtHb677Fy6QBLxJtQ0wsT739BYdNIkc8ZNnzmveHIB-II5T2t3Gywp_XDKfx4-rEyaMbxZEZ94Q4DIFJxWMwOYEuNJzcbrez44cwqg&s=10',
   };
+
+  alterarFavorito() {
+    this.livro.favorito = !this.livro.favorito;
+  }
 }
