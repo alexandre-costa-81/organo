ce51da099e5cd182b7c17bb97a1e35824d8a2dce
Alexandre Costa <alexandre.costa@furg.br>
2025-11-01 13:54:46 -0300

    Add ListaLivros component with HTML, CSS, and test files; update app structure to include ListaLivros

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Cabecalho } from "./componentes/cabecalho/cabecalho";
 import { Rodape } from "./componentes/rodape/rodape";
 import { Livro } from "./componentes/livro/livro";
+import { ListaLivros } from "./componentes/lista-livros/lista-livros";
 
 @Component({
   selector: 'app-root',
-  imports: [Cabecalho, Rodape, Livro],
+  imports: [Cabecalho, Rodape, ListaLivros],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
