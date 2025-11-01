e687c992afc68827dc74280fa9dd514d1b5744f4
Alexandre Costa <alexandre.costa@furg.br>
2025-11-01 14:17:36 -0300

    Refactor code structure for improved readability and maintainability

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Cabecalho } from "./componentes/cabecalho/cabecalho";
 import { Rodape } from "./componentes/rodape/rodape";
-import { Livro } from "./componentes/livro/livro";
 import { ListaLivros } from "./componentes/lista-livros/lista-livros";
+import { GeneroLiterario } from "./componentes/genero-literario/genero-literario";
 
 @Component({
   selector: 'app-root',
-  imports: [Cabecalho, Rodape, ListaLivros],
+  imports: [Cabecalho, Rodape, ListaLivros, GeneroLiterario],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
